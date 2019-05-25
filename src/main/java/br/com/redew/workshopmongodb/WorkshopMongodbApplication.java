43cb2e4ff206c4bed4f8d44a8781964819144da8
weder-monteiro <weder_monteiro@outlook.com.br>
2019-05-25 17:46:22 -0300

    Primeiro commit

---
+++ src/main/java/br/com/redew/workshopmongodb/WorkshopMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.redew.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongodbApplication.class, args);
+	}
+
+}
