c0991d1db37589116e38b75aee484bd2cc5a16f7
weder-monteiro <weder_monteiro@outlook.com.br>
2019-06-02 20:54:00 -0300

    Consulta simples com @Query

--- src/main/java/br/com/redew/workshopmongodb/repository/PostRepository.java
+++ src/main/java/br/com/redew/workshopmongodb/repository/PostRepository.java
@@ -3,12 +3,16 @@ package br.com.redew.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.redew.workshopmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>  {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
