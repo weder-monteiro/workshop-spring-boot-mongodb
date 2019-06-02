deede5cc77626a9a2a53bb14409b7c92d55769be
weder-monteiro <weder_monteiro@outlook.com.br>
2019-06-02 16:24:24 -0300

    Projecao dos dados do autor com DTO

---
+++ src/main/java/br/com/redew/workshopmongodb/dto/AuthorDTO.java
@@ -0,0 +1,37 @@
+package br.com.redew.workshopmongodb.dto;
+
+import java.io.Serializable;
+
+import br.com.redew.workshopmongodb.domain.User;
+
+public class AuthorDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	
+	public AuthorDTO() {
+		
+	}
+	
+	public AuthorDTO(User user) {
+		this.id = user.getId();
+		this.name = user.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
