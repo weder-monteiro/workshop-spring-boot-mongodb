fe15e6fcfc3eee9952438927fef0a3f1f75513af
weder-monteiro <weder_monteiro@outlook.com.br>
2019-06-02 15:42:33 -0300

    Criando entity Post com user aninhado

---
+++ src/main/java/br/com/redew/workshopmongodb/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package br.com.redew.workshopmongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.redew.workshopmongodb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>  {
+	
+}
