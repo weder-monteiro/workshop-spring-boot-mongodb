56ef79f49ea9f5afbd8aed54e7033a2550586fa4
weder-monteiro <weder_monteiro@outlook.com.br>
2019-06-02 19:57:10 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/redew/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package br.com.redew.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.redew.workshopmongodb.domain.Post;
+import br.com.redew.workshopmongodb.repository.PostRepository;
+import br.com.redew.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
