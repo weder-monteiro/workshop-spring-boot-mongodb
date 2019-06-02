5ea4a799a1ccba9792527b893ae5d9e0fb9f2a1d
weder-monteiro <weder_monteiro@outlook.com.br>
2019-06-02 17:00:33 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/br/com/redew/workshopmongodb/resources/UserResource.java
+++ src/main/java/br/com/redew/workshopmongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.redew.workshopmongodb.domain.Post;
 import br.com.redew.workshopmongodb.domain.User;
 import br.com.redew.workshopmongodb.dto.UserDTO;
 import br.com.redew.workshopmongodb.services.UserService;
@@ -60,5 +61,12 @@ public class UserResource {
 		user.setId(id);
 		user = service.update(user);
 		return ResponseEntity.noContent().build(); //Operação e não tem que retornar nada resposta e 204 
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET) // ou @GetMapping
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
