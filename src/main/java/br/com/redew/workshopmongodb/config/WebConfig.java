f7eb30f1e2205a8f938c74c65fe433628887763d
Weder Monteiro Da Silva <wmsilva4@stefanini.com>
2019-07-29 13:00:41 -0300

    Permitindo acesso ao app angular no heroku

--- src/main/java/br/com/redew/workshopmongodb/config/WebConfig.java
+++ src/main/java/br/com/redew/workshopmongodb/config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:4200")
+            .allowedOrigins("https://consumindo-api-angular.herokuapp.com")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
             .allowCredentials(true);
 	}
